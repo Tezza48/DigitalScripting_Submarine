0daae9ff1688ea3f1f32eb3bafe9254843a84cb1
William Terry <Tezza48is@aol.com>
2015-10-05 19:55:52 +0100

    Infinitely spawning Coral sections
    sections now spawn as you move foreword.
    
    TODO make randomisation script.

--- Jay Will Submarine 1/Assets/Jay/Scr/playerMovement.js
+++ Jay Will Submarine 1/Assets/Jay/Scr/playerMovement.js
@@ -3,7 +3,6 @@
 
 
 function Update(){
-		transform.position =Vector3(Input.GetAxis("Horizontal") * distance, Input.GetAxis("Vertical") * distance, -10);
+		transform.position.x = Input.GetAxis("Horizontal") * distance;
+		transform.position.y = Input.GetAxis("Vertical") * distance;
 }
-
-
---
+++ Jay Will Submarine 1/Assets/Will/Scr/spawnNextSegment.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var nextLocation : float = 20;
+var section : GameObject;
+var hasSpawnedNextSection : boolean = false;
+var obstacles : GameObject;
+
+function Start () {
+        obstacles = GameObject.Find("obstacles");
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+        if (!hasSpawnedNextSection){
+                var next = Instantiate(section, Vector3(transform.position.x, transform.position.y, transform.position.z + nextLocation), transform.rotation);
+                next.name = "Section";
+                next.transform.parent = obstacles.transform;
+                hasSpawnedNextSection = true;
+        }
+}
