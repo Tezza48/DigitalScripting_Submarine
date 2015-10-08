77f655a45743631788ac17685b14efb89dfed2a0
god-dammit-will <jaypointer9@gmail.com>
2015-10-08 16:24:44 +0100

    Added Movement along the z Axis

--- Jay Will Submarine 1/Assets/Jay/Scr/playerMovement.js
+++ Jay Will Submarine 1/Assets/Jay/Scr/playerMovement.js
@@ -1,8 +1,11 @@
 ﻿var distance : float =10;
-
-
+var speed :float =10;
+var xPos :float;
+var yPos :float;
+var zPos :float;
 
 function Update(){
 		transform.position.x = Input.GetAxis("Horizontal") * distance;
 		transform.position.y = Input.GetAxis("Vertical") * distance;
+		transform.position.z += speed * Time.deltaTime;
 }
