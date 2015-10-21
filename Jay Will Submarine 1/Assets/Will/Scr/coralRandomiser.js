c6091147f6421d7ebd1e53d0bfab96d9ee7c0a73
William Terry <Tezza48is@aol.com>
2015-10-21 18:01:56 +0100

    random works

--- Jay Will Submarine 1/Assets/Will/Scr/coralRandomiser.js
+++ Jay Will Submarine 1/Assets/Will/Scr/coralRandomiser.js
@@ -2,14 +2,26 @@
 
 var coral = new GameObject[9];
 
+function Awake () {
+	//get children and put in array
+	//FillCoral();
+	RandomiseCoral();
+}
+
+function FillCoral () {
+	var j = 0;
+	for (var i = 0; i < transform.childCount; i++) {
+		if (transform.GetChild(i).name == "Coral") {
+			coral[i] = transform.GetChild(j).gameObject;
+			j++;
+		}
+	}
+}
+
 function RandomiseCoral () {
       for (i in coral) {
             var rng : float = Random.value;
             var spawn : boolean = (rng > 0.7f) ? true : false;
-            if (!spawn) i.SetActive(false);
+            i.SetActive(spawn ? true : false);
       }
-}
-
-function Start () {
-      RandomiseCoral();
 }
